da16b93c35861b69825ca9035d5962b6f368875b
Tadea <tsimunoviczg@gmail.com>
2020-04-20 22:37:13 -0400

    Updates

---
+++ precache-manifest.682ed5e2040929da3d06531ede53668e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9193fd41e647bd2908fc897bd58185fb",
+    "url": "/weather-today/index.html"
+  },
+  {
+    "revision": "ef4546381be498e6dedb",
+    "url": "/weather-today/static/css/2.e5adc83a.chunk.css"
+  },
+  {
+    "revision": "ac8ae59027aa560a9068",
+    "url": "/weather-today/static/css/main.e7d5af0b.chunk.css"
+  },
+  {
+    "revision": "ef4546381be498e6dedb",
+    "url": "/weather-today/static/js/2.5738d831.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weather-today/static/js/2.5738d831.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ac8ae59027aa560a9068",
+    "url": "/weather-today/static/js/main.c3c37c57.chunk.js"
+  },
+  {
+    "revision": "2533369c75373797913c",
+    "url": "/weather-today/static/js/runtime-main.685cf6ed.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/weather-today/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/weather-today/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/weather-today/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/weather-today/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/weather-today/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "71f57de6d85cfdb7c637465a88865d92",
+    "url": "/weather-today/static/media/sun.71f57de6.jpg"
+  }
+]);
