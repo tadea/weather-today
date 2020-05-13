1414db84cd6a9d0517cb20584ce32f2f95d5143a
Tadea <tsimunoviczg@gmail.com>
2020-05-13 11:13:47 -0400

    chaged Form.js to const from Class

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,15 @@
 import React from "react";
 
-class Form extends React.Component {
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.props.getWeather}>
-          <input type="text" name="city" placeholder="City" />
-          <input type="text" name="country" placeholder="Country" />
-          <button>Get Weather</button>
-        </form>
-      </div>
-    );
-  }
-}
+const Form = (props) => {
+  return (
+    <div>
+      <form onSubmit={props.getWeather}>
+        <input type="text" name="city" placeholder="City" />
+        <input type="text" name="country" placeholder="Country" />
+        <button>Get Weather</button>
+      </form>
+    </div>
+  );
+};
 
 export default Form;
