1d2409f2c4ec04af5bdf69bc08634cc667e67e72
Tadea <tsimunoviczg@gmail.com>
2020-05-15 10:16:06 -0400

    changed css and imported App.css

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../App.css";
 
 class Weather extends React.Component {
   handleClicked() {
@@ -12,86 +13,96 @@ class Weather extends React.Component {
 
   render() {
     return (
-      <div className="weather__info">
-        {this.props.city && this.props.country && (
-          <p className="weather__key">
-            {" "}
-            <i className="fas fa-location-arrow"></i> Location:{" "}
-            <span className="weather__value">
-              {this.props.city}, {this.props.country}
-            </span>
-          </p>
-        )}
-
-        {this.props.kelvinTemp && (
-          <p className="weather__key">
-            <i className="fas fa-thermometer-three-quarters"></i> Temperature:{" "}
-            <span
-              onClick={this.handleClicked.bind(this)}
-              className="weather__value"
-            >
-              {this.calculateTemp(
-                this.props.kelvinTemp,
-                this.props.displayUnits
-              )}
-              &deg;
-              {this.props.displayUnits}
-            </span>
-          </p>
-        )}
-        {this.props.tempMin && this.props.tempMax && (
-          <p className="weather__key">
-            {" "}
-            <i className="fas fa-sort"></i> Min/Max:{" "}
-            <span
-              onClick={this.handleClicked.bind(this)}
-              className="weather__value"
-            >
-              {this.calculateTemp(this.props.tempMin, this.props.displayUnits)}
-              &deg;{this.props.displayUnits} |
-              {this.calculateTemp(this.props.tempMax, this.props.displayUnits)}
-              &deg;
-              {this.props.displayUnits}
-            </span>
-          </p>
-        )}
-        {this.props.humidity && (
-          <p className="weather__key">
-            {" "}
-            <i className="fas fa-tint"></i> Humidity:{" "}
-            <span className="weather__value">{this.props.humidity}%</span>
-          </p>
-        )}
-        {this.props.description && (
-          <p className="weather__key">
-            {" "}
-            <i className="fas fa-bars"></i> Condition:{" "}
-            <span className="weather__value">{this.props.description}</span>
-          </p>
-        )}
-        {this.props.pressure && (
-          <p className="weather__key">
-            {" "}
-            <i className="fas fa-wind"></i> Pressure:{" "}
-            <span className="weather__value">{this.props.pressure} hPa</span>
-          </p>
-        )}
-
-        {this.props.error && (
-          <p className="weather__key">
-            {" "}
-            <i className="fas fa-exclamation"></i>{" "}
-            <span className="weather__value">{this.props.error}</span>
-          </p>
-        )}
-        {this.props.icon && (
-          <p>
-            <img
-              src={`http://openweathermap.org/img/w/${this.props.icon}.png`}
-              alt="wthr img"
-            />
-          </p>
-        )}
+      <div className="container">
+        <div className="cards pt-4">
+          <h1>
+            {this.props.city && this.props.country && (
+              <p>
+                {this.props.city}, {this.props.country}
+              </p>
+            )}
+          </h1>
+          {this.props.icon && (
+            <p>
+              <img
+                src={`http://openweathermap.org/img/w/${this.props.icon}.png`}
+                alt="wthr img"
+                size="40px"
+              />
+            </p>
+          )}
+          <h1 className="py-2">
+            {this.props.kelvinTemp && (
+              <p>
+                <i className="fas fa-thermometer-three-quarters"></i>{" "}
+                <span onClick={this.handleClicked.bind(this)}>
+                  {this.calculateTemp(
+                    this.props.kelvinTemp,
+                    this.props.displayUnits
+                  )}
+                  &deg;
+                  {this.props.displayUnits}
+                </span>
+              </p>
+            )}
+          </h1><br></br>
+          <h4 className="py-6">
+            {this.props.description && (
+              <p>
+                {" "}
+                <i className="fas fa-info"></i>{" "}
+                <span>{this.props.description}</span>
+              </p>
+            )}
+          </h4>
+          <h4 className="py-3">
+            {this.props.tempMin && this.props.tempMax && (
+              <p>
+                {" "}
+                <i className="fas fa-sort"></i> Min/Max:{" "}
+                <span onClick={this.handleClicked.bind(this)}>
+                  {this.calculateTemp(
+                    this.props.tempMin,
+                    this.props.displayUnits
+                  )}
+                  &deg;{this.props.displayUnits} |
+                  {this.calculateTemp(
+                    this.props.tempMax,
+                    this.props.displayUnits
+                  )}
+                  &deg;
+                  {this.props.displayUnits}
+                </span>
+              </p>
+            )}
+          </h4>
+          <h4 className="py-4">
+            {this.props.humidity && (
+              <p>
+                {" "}
+                <i className="fas fa-water"></i> Humidity:{" "}
+                <span>{this.props.humidity}%</span>
+              </p>
+            )}
+          </h4>
+          <h4 className="py-5">
+            {this.props.pressure && (
+              <p>
+                {" "}
+                <i className="fas fa-tachometer-alt"></i> Pressure:{" "}
+                <span>{this.props.pressure} hPa</span>
+              </p>
+            )}
+          </h4>
+          <h4>
+            {this.props.error && (
+              <p>
+                {" "}
+                <i></i> <span>{this.props.error}</span>
+              </p>
+            )}
+          </h4>
+        </div>
       </div>
     );
   }
