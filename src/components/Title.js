bd493978f1af2f72c25b98422380d56a4cc7c1d6
Tadea <tsimunoviczg@gmail.com>
2020-05-15 10:16:35 -0400

    removed Clock component and changed css

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,18 +1,13 @@
 import React from "react";
-import Clock from "./Clock";
+import "../App.css";
 
 class Title extends React.Component {
   state = {};
   render() {
     return (
-      <div>
-        <h5 className="title-container__clock">
-          <Clock />
-        </h5>
-        <h1 className="title-container__title">Weather Today</h1>
-        <p className="title-container__subtitle">
-          Get the latest weather conditions
-        </p>
+      <div className="container">
+        <h1>Weather Today</h1>
+        <p>Get the latest weather conditions</p>
       </div>
     );
   }
