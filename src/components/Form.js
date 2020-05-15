3f87319e4b929953765e6cd8a93befb5484f3628
Tadea <tsimunoviczg@gmail.com>
2020-05-15 10:16:55 -0400

    changed css

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,31 @@
 import React from "react";
+import "../form.css";
 
 const Form = (props) => {
   return (
-    <div>
+    <div className="container">
       <form onSubmit={props.getWeather}>
-        <input type="text" name="city" placeholder="City" />
-        <input type="text" name="country" placeholder="Country" />
-        <button>Get Weather</button>
+        <div className="row">
+          <div className="col-md-3 offset-md-2">
+            <input
+              type="text"
+              name="city"
+              className="form-control"
+              placeholder="City"
+            />
+          </div>
+          <div className="col-md-3">
+            <input
+              type="text"
+              name="country"
+              className="form-control"
+              placeholder="Country"
+            />
+          </div>
+          <div className="col-md-3 mt-md-0 mt-2 text-md-left">
+            <button className="btn btn-default">Get Weather</button>
+          </div>
+        </div>
       </form>
     </div>
   );
